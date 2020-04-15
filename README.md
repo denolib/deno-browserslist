17a7aa7bd8ddd5161d95b8b3f79ab8ddaa50cf3a
Nick Mudge <nick@perfectabstractions.com>
2020-04-15 03:47:46 -0400

    Port

---
+++ README.md
@@ -0,0 +1,8 @@
+# deno-browserslist
+
+Port of browserslist to deno.
+
+Does not contain electron data or nodejs data. Only browser data.
+
+Version [4.11.1](https://github.com/browserslist/browserslist/releases/tag/4.11.1)
+
---
+++ deps.js
@@ -0,0 +1,9 @@
+//export * as mozilla from "https://raw.githubusercontent.com/denolib/deno-source-map/master/mod.js";
+
+export * as path from "https://deno.land/std/node/path.ts";
+
+//export * as chalk from "https://deno.land/std/fmt/colors.ts";
+
+export * as fs from "https://deno.land/std/node/fs.ts";
+
+export * as caniuselite from "https://raw.githubusercontent.com/denolib/deno-caniuse-lite/master/mod.js";
